3bf820fb25746c9a8c40d450694ccb44bea87386
James Swank <jmswank7@gmail.com>
2019-05-23 13:42:11 -0600

    armstrong numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong_number(number):
-    pass
+    digits = [int(x) for x in str(number)]
+    digit_count = len(digits)
+    # if sum([(y**digit_count) for y in digits]) == number:
+    #     return True
+    # else:
+    #     return False
+    return sum([(y**digit_count) for y in digits]) == number
