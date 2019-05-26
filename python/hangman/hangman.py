463c7eb9055e52a6979ef3da85992e28711e2aa9
James Swank <jmswank7@gmail.com>
2019-05-26 17:48:31 -0600

    new verion start

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -5,34 +5,55 @@ STATUS_LOSE = "lose"
 STATUS_ONGOING = "ongoing"
 
 guesses = []
-
-
+alpha = list('abcdefghijklmnopqrstuvwxyz')
 word = list(input("Enter word: "))
-print(word)
+letters = ''
+for letter in word:
+    letters += '_' 
+print(letters)
+
+# print(pre_answer)
 
 
-class Hangman(object):
-    def __init__(self, word):
-        self.remaining_guesses = 9
-        self.status = STATUS_ONGOING
+
+
+
+
+
+
+# class Hangman(object):
+#     def __init__(self, word):
+#         self.remaining_guesses = 9
+#         self.status = STATUS_ONGOING
         
-
-    def guess(self, char):
         
-        guesses.insert(0,char)
-        self.remaining_guesses -= 1
-        
-        print(guesses)
+#     def guess(self, char):
+#         guesses.insert(0,char)
+#         if char in word:
+#             print('correct')
+#             self.remaining_guesses -= 1
+#         else:
+#             print('incorrect')
+#             self.remaining_guesses -= 1
+#         print("You have guessed {}:".format(guesses))
+#         print("You have {} guesses left".format(self.remaining_guesses))
+       
 
-    def get_masked_word(self):
-        pass
+#     def get_masked_word(self):
+#         pass
 
-    def get_status(self):
-        
-        print(word)
+#     def get_status(self):
+#         pass
+
+# def play():
+#     a = Hangman(word)
+#     a.guess(input("Enter letter: "))
+
+# play()
 
 
 
-# a = Hangman(word)
-# a.guess(input("Guess letter: "))
+
+
+
        
