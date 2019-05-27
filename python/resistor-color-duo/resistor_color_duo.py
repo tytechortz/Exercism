28117d61e8ddeb24b6ffcbb8b9447948f20d379f
James Swank <jmswank7@gmail.com>
2019-05-27 15:45:38 -0600

    finishes resistor duo

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+def value(value):
+    return colors().index(value[0])*10 + colors().index(value[1])
+
+def colors():
+    return  [
+            'black',
+            'brown',
+            'red',
+            'orange',
+            'yellow',
+            'green',
+            'blue',
+            'violet',
+            'grey',
+            'white'
+        ]
+
