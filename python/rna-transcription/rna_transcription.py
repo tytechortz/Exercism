4c95c0bee966d3733ad7f1e0e15c49943c34c2c4
James Swank <jmswank7@gmail.com>
2019-05-28 14:28:08 -0600

    refactor

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,9 +1,10 @@
 def to_rna(dna_strand):
     split_rna = [i for i in dna_strand]
     rna_trans = {'C':'G', 'G':'C','T':'A','A':'U'}
-    rna_strand = ''.join(([rna_trans.get(l, l) for l in split_rna]))
-    return rna_strand
+    rna_strand = ''.join(([rna_trans[l] for l in split_rna]))
+    print(rna_strand)
+
+to_rna('ACGTGGTCTTAA')
 
 
     
-to_rna('ACGTGGTCTTAA')  
