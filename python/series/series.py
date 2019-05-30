373870091b9744c4dbff1569b51316520e733676
James Swank <jmswank7@gmail.com>
2019-05-30 13:46:30 -0600

    passes tests

---
+++ python/series/series.py
@@ -0,0 +1,17 @@
+def slices(series, length):
+    series_length = len(series)
+    results = []
+    if series_length < length:
+        raise ValueError('slice length too large')
+    elif length == 0:
+        raise ValueError("slice can't be 0")
+    elif length < 0:
+        raise ValueError("slice can't be negative")
+    elif series == '':
+        raise Exception("can't use empty series")
+    else:
+        n=0
+        while n+length < series_length + 1:
+            results.append(series[n:n+length])
+            n += 1
+    return results
