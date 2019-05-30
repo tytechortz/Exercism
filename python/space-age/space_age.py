d0731446cc48f5a20c3ffa05fa76c869beb3d638
James Swank <jmswank7@gmail.com>
2019-05-30 12:24:26 -0600

    refactor

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,29 +1,30 @@
 class SpaceAge(object):
     def __init__(self, seconds):
         self.seconds = seconds
+        self.earthYears = self.seconds / 31557600
         
  
     def on_earth(self):
-        return round(self.seconds/31557600, 2)
+        return round(self.earthYears, 2)
             
     def on_mercury(self):
-        return round((self.seconds/31557600 / .2408467), 2)
+        return round((self.earthYears / .2408467), 2)
 
     def on_venus(self):
-        return round((self.seconds/31557600 / .61519726), 2)
+        return round((self.earthYears / .61519726), 2)
 
     def on_mars(self):
-        return round((self.seconds/31557600 / 1.8808158), 2) 
+        return round((self.earthYears / 1.8808158), 2) 
 
     def on_jupiter(self):
-        return round((self.seconds/31557600 / 11.862615), 2)
+        return round((self.earthYears / 11.862615), 2)
 
     def on_saturn(self):
-        return round((self.seconds/31557600 / 29.447498), 2) 
+        return round((self.earthYears / 29.447498), 2) 
 
     def on_uranus(self):
-        return round((self.seconds/31557600 / 84.016846), 2)
+        return round((self.earthYears / 84.016846), 2)
 
     def on_neptune(self):
-        return round((self.seconds/31557600 / 164.79132), 2)
+        return round((self.earthYears / 164.79132), 2)
 
