082cd0ee70310cb9ca4f26fe40f417528caec6fe
James Swank <jmswank7@gmail.com>
2019-05-30 09:38:42 -0600

    passes tests

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,13 @@
+def latest(scores):
+    return(scores[-1])
+
+
+def personal_best(scores):
+    return(max(scores))
+
+
+def personal_top_three(scores):
+    sorted_scores = sorted(scores, reverse=True)
+    return(sorted_scores[:3])
+
+
