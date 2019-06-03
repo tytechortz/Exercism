bc0f0d19412ecb662a2ee206e8f62bd4784528fe
James Swank <jmswank7@gmail.com>
2019-06-02 18:29:19 -0600

    converts to ints

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,18 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        # # self.matrix = matrix_string.split()
-        # self.matrix = matrix_string.split('\n')
-        # # for i in self.matrix: 
-        # #     ",".join(self.matrix)
-        # print(self.matrix)
-        self.matrix = matrix_string.splitlines()
+        self.matrix = []
+        self.string = matrix_string.splitlines()
+        for i in self.string:
+            self.matrix.append(i.split(' '))
+
+        self.matrix = [[int(j) for j in k] for k in self.matrix]
+       
+
+        # self.matrix.split()
+        # print(self.string[0])
+        print(self.matrix)
+
+
 
     def row(self, index):
         # print(self.matrix[index-1])
@@ -17,7 +24,10 @@ class Matrix(object):
         
 
     def column(self, index):
-        pass
+        answer = []
+        # for i in self.matrix[:,index-1].split():
+        #     answer.append(i)
+        print(self.matrix)
 
-# m = Matrix("1 2 3\n4 5 6\n7 8 9\n18 7 6")
-# m.row(3)
+m = Matrix("1 2 3\n4 5 6\n7 8 9\n18 7 6")
+# m.column(3)
