d9c52949e46f16527ce7e578f0081becffe72670
James Swank <jmswank7@gmail.com>
2019-06-02 22:44:04 -0600

    passes tests

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,10 +1,29 @@
 def is_valid(isbn):
-    print(isbn[-1])
+    isbn = list(isbn)
+    # print(isbn)
+    if len(isbn) == 0:
+        return False
+    isbn[:] = [x for x in isbn if x != '-']
+    # print(isbn)
     if isbn[-1] == 'X':
-        new_isbn = isbn.replace(isbn[-1], '10', 1)
+        isbn[-1] = '10'
+    # print(isbn)
+    # isbn = ''.join(map(str, isbn))
+    # isbn = list(isbn)
+    # print(isbn)
+    isbn = [x for x in isbn if x.isdigit()]
+    isbn = [int(i) for i in isbn]
+    # print(isbn[0])
+    # print(len(isbn))
+    if len(isbn) == 10:
+        if (isbn[0] * 10 + isbn[1] * 9 + isbn[2] * 8 + isbn[3] * 7 + isbn[4] * 6 + isbn[5] * 5 + isbn[6] * 4 + isbn[7] * 3 + isbn[8] * 2 + isbn[9] * 1) % 11 == 0:
+            # print('good isbn')
+            return True
+        else:
+            # print('invalid')
+            return False
+    else:
+        # print('invalid')
+        return False
 
-    # isbn = (''.join(i for i in isbn if i.isdigit()))
-    
-    print(new_isbn)
-
-is_valid('3-598-21508-X')
+# is_valid('3-598-P1581-X')
