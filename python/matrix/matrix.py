482ef5338d0ae278f188c0f49c0bcaf04e76b0b5
James Swank <jmswank7@gmail.com>
2019-06-02 19:31:50 -0600

    passes tests

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -7,27 +7,16 @@ class Matrix(object):
 
         self.matrix = [[int(j) for j in k] for k in self.matrix]
        
-
-        # self.matrix.split()
-        # print(self.string[0])
-        print(self.matrix)
-
-
-
     def row(self, index):
-        # print(self.matrix[index-1])
         answer = []
         for i in self.matrix[index-1]:
             answer.append(int(i))
-        print(answer)
         return answer
         
 
     def column(self, index):
-        answer = []
-        # for i in self.matrix[:,index-1].split():
-        #     answer.append(i)
-        print(self.matrix)
+        return [row[index-1] for row in self.matrix]
 
-m = Matrix("1 2 3\n4 5 6\n7 8 9\n18 7 6")
+m = Matrix("1")
 m.row(3)
+m.column(1)
