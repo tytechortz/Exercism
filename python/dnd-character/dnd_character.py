983f301898c4b28a805e8310f65b9b0a831fb2a1
James Swank <jmswank7@gmail.com>
2019-06-03 18:01:42 -0600

    creates dict of abilities and scores

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,17 +1,39 @@
-import numpy
+import numpy as np
 
 class Character:
-    def __init__(self):
-        pass
-    def rand_gen(self):
-        dice = numpy.random.randint(1,6,4)
+    # def __init__(self):
+    #     pass
+    def __init__(self, strength, dexterity, constitution, intelligence, wisdom, charisma, hitpoints):
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
+        self.hitpoints = hitpoints
 
-        # for i in range(4):
-        #     rand_list = numpy.random.randint(1,6,4)
-        #     dice.append(rand_list)
+    def dice(self):
+        abilities = ['self.strength', 'self.dexterity', 'self.constitution', 'self.intelligence', 'self.wisdom', 'self.charisma']
+        scores = []
+        for x in range(len(abilities)):
 
-        print(dice)
+        # abilities = {self.strength: 0, 'self.dexterity': 0, 'self.constitution': 0, 'self.intelligence': 0, 'self.wisdom': 0, 'self.charisma': 0}
+            dice = np.random.randint(1,6,4)
+            dice[::-1].sort()
+            dice = np.delete(dice, -1)
+            # print(dice)
+            points = sum(dice)
+            scores.append(points)
+        # return points
+        # abilities_new = {k: points for k, points in abilities.items()}
+            
+        print(abilities)
+        print(scores)
+        d = {k:v for k,v in zip(abilities,scores)}
+        for k, v in d.items():
+            print(k,v)
 
-c1 = Character()
+    
+c1 = Character(0,0,0,0,0,0,0)
 
-c1.rand_gen()
+c1.dice()
