ca6ef432f4eb7e3e11cc5d14a0de689d06ed56bd
James Swank <jmswank7@gmail.com>
2019-06-25 15:27:44 -0600

    maybe works

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,7 +1,17 @@
 def find_anagrams(word, candidates):
     chars = [char for char in word]
+    chars = ''.join(sorted(chars))
     print(chars)
-    print(''.join(sorted(chars)))
+    print(candidates)
+    sorted_candidates = []
+    answer = []
+    for x in candidates:
+        sorted_candidates.append(''.join(sorted(x)))
+    print(sorted_candidates)
+    for y in range(len(sorted_candidates)):
+        if sorted_candidates[y] == chars:
+            answer.append(candidates[y])
+    print(answer)
     
 
-find_anagrams("master", ["stream", "maters"])
+find_anagrams("master", ["stream", "maters", "bluff"])
