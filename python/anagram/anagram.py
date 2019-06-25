572d7cc4639fbbb15735c010bc5448fed4b5ab71
James Swank <jmswank7@gmail.com>
2019-06-25 16:22:34 -0600

    passes tests

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,17 +1,22 @@
 def find_anagrams(word, candidates):
-    chars = [char for char in word]
+    
+    chars = [char for char in word.lower()]
+    # chars1 = ''.join(chars.sort(key=lambda x: x.lower()))
     chars = ''.join(sorted(chars))
+    
     print(chars)
-    print(candidates)
+    print(candidates[0])
     sorted_candidates = []
     answer = []
     for x in candidates:
-        sorted_candidates.append(''.join(sorted(x)))
+        sorted_candidates.append(''.join(sorted(x.lower())))
     print(sorted_candidates)
     for y in range(len(sorted_candidates)):
-        if sorted_candidates[y] == chars:
+        if sorted_candidates[y] == chars and candidates[y] != word and candidates[y].lower() != word.lower():
             answer.append(candidates[y])
     print(answer)
+    return(answer)
+   
     
 
-find_anagrams("master", ["stream", "maters", "bluff"])
+find_anagrams("BANANA", ["BANANA", "Banana", "banana"])
