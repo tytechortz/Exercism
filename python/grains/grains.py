3745c4b5b62c3bb963d28edb7c23fbb036f4ad9d
James Swank <jmswank7@gmail.com>
2019-06-26 13:22:32 -0600

    passes tests

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,9 +1,22 @@
 def square(number):
-    print(int(2**number/2))
+    if number < 1 or number > 64:
+        raise ValueError("number mjust be between 1 and 64")
+    else:
+        print(int(2**number/2))
+        return int(2**number/2)
 
 
 def total(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError("number mjust be between 1 and 64")
+
+    else:
+    # for x in range(1,number+1):
+        fun = 2**number - 1
+        
+    print(fun)
+    return(fun)
 
 
-square(64)
+square(4)
+total(20)
