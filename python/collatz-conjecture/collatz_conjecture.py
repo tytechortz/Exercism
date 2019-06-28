ade5da0eab64f05c5ae1fe31312c810f9adead05
James Swank <jmswank7@gmail.com>
2019-06-27 18:50:47 -0600

    passes tests

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,3 +1,13 @@
 def steps(number):
+    step = 0
     if number < 1:
         raise ValueError("number must be 1 or greater")
+    else:
+        while number != 1:
+            if number % 2 == 0:
+                number = number / 2
+                step += 1
+            else:
+                number = number * 3 + 1
+                step += 1
+    return step
