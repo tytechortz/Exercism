b7bc7859493169debcb9148bfbc09c7083b928da
James Swank <jmswank7@gmail.com>
2019-06-28 13:35:41 -0600

    convert to np array, print elements

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,16 @@
+import numpy as np
+
 def saddle_points(matrix):
-    pass
+    m = np.array(matrix)
+    sp = []
+    rows = m.shape[0]
+    cols = m.shape[1]
+    print(m)
+    for x in range(0, rows):
+        for y in range(0, cols):
+            print(m[x,y])
+    
+
+
+
+saddle_points([[9, 8, 7], [5, 3, 2], [6, 6, 7]])
