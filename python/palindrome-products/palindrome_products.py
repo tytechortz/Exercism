5d1020951f1510f2fcdab2ac45e9e428f396ac5c
James Swank <jmswank7@gmail.com>
2019-06-28 17:20:01 -0600

    quits saddle points, starts palindrome products

---
+++ python/palindrome-products/palindrome_products.py
@@ -0,0 +1,11 @@
+def largest(min_factor, max_factor):
+    pass
+
+
+def smallest(min_factor, max_factor):
+    for x in range(min_factor,max_factor+1):
+        print(x)
+
+
+largest(1,9)
+smallest(1,9)
