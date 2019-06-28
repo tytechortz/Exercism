e58ea5cedab1e74023ec5e3415b8c747f8538f7a
James Swank <jmswank7@gmail.com>
2019-06-28 17:54:32 -0600

    makes list of range

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,10 +1,14 @@
+import itertools
+
 def largest(min_factor, max_factor):
     pass
 
 
 def smallest(min_factor, max_factor):
-    for x in range(min_factor,max_factor+1):
-        print(x)
+    x = [i for i in range(max_factor+1)]
+    # x.append(i) for i in range(min_factor,max_factor+1)
+    print(x)
+    # list(itertools.permutations(x)
 
 
 largest(1,9)
