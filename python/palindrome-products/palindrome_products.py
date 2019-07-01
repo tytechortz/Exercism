ddc595d6c0f4e79a033da4b24b7d1516d6105513
James Swank <jmswank7@gmail.com>
2019-07-01 09:58:38 -0600

    gets list of palindromes

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,24 +1,33 @@
-import itertools
-
 def largest(min_factor, max_factor):
     pass
 
 
 def smallest(min_factor, max_factor):
     x = [i for i in range(1,max_factor+1)]
-    # x.append(i) for i in range(min_factor,max_factor+1)
-    print(x)
+    # print(x)
     products = []
+    palindromes = []
+    num_str = []
     for j in x:
         for k in x:
-
             products.append(j*k)
-    # products = list(itertools.permutations(x))
         
-    print(products)
+    # print(products)
     s = list(dict.fromkeys(products))
     print(s)
+    for number in s:
+        num_str.append(str(number))
+    print(num_str)
+
+    print(num_str[-1])
+    print(num_str[-1][::-1])
     
+    for digits in num_str:
+        # print(digits)
+        if digits[::-1] == digits:
+            palindromes.append(digits)
+    print(palindromes)
+
 
 largest(1,9)
 smallest(1,9)
