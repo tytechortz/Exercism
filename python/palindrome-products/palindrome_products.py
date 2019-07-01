071c508960ae0982d79d9c3b4c559e5c6798e285
James Swank <jmswank7@gmail.com>
2019-06-30 18:47:10 -0600

    gets list of products

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -5,11 +5,20 @@ def largest(min_factor, max_factor):
 
 
 def smallest(min_factor, max_factor):
-    x = [i for i in range(max_factor+1)]
+    x = [i for i in range(1,max_factor+1)]
     # x.append(i) for i in range(min_factor,max_factor+1)
     print(x)
-    # list(itertools.permutations(x)
+    products = []
+    for j in x:
+        for k in x:
 
+            products.append(j*k)
+    # products = list(itertools.permutations(x))
+        
+    print(products)
+    s = list(dict.fromkeys(products))
+    print(s)
+    
 
 largest(1,9)
 smallest(1,9)
