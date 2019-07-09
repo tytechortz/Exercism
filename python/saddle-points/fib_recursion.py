7df041e8b96c060b74a7f5e776746ea7c88b019d
James Swank <jmswank7@gmail.com>
2019-07-08 19:06:33 -0600

    passes

--- python/saddle-points/fib_recursion.py
+++ python/saddle-points/fib_recursion.py
@@ -1,7 +1,6 @@
 def get_fib(position):
     if position == 0 or position == 1:
-        return position
-        
+        return position   
     return get_fib(position - 1) + get_fib(position -2)
 
 # Test cases
