9804e0b2011b1c891d3780537e302063e6913160
James Swank <jmswank7@gmail.com>
2019-07-18 09:31:48 -0600

    finsihes palindrome algorithm

--- python/saddle-points/rec_palindromes.py
+++ python/saddle-points/rec_palindromes.py
@@ -1,2 +1,11 @@
 def isPalindrome(word):
+    if len(word) <= 1:
+        return True
+
+    if word[0] != word[len(word) - 1]:
+        return False
+
     
+    return isPalindrome(word[1:-1])
+
+print(isPalindrome('noon'))
