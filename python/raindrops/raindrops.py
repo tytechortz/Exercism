546d4b9a722de568ca8e31334489ef7c9bce14e4
James Swank <jmswank7@gmail.com>
2019-07-31 13:09:33 -0600

    might work

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,11 +1,27 @@
 def convert(number):
     factors = []
+    drops = []
+    listOfFactors = [3,5,7]
     for i in range(1, number + 1):
         if number % i == 0:
             factors.append(i)
 
     print(factors)
+    check = any(item in factors for item in listOfFactors)
+
+    if check is True:
+        for j in factors:
+            if j == 3:
+                drops.append("Pling")
+            elif j == 5:
+                drops.append("Plang")
+            elif j == 7:
+                drops.append("Plong")
+        
+        seperator = ''
+        print(seperator.join(drops))
+    else:
+        print(number)
 
 
-
-convert(15)
+convert(105)
