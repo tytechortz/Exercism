9aaaf46c47d4fd69586c35548c8b08cd2c51ad19
James Swank <jmswank7@gmail.com>
2019-08-02 16:38:47 -0600

    refactor works for 2

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,21 +1,10 @@
 def factors(value):
-    p_factors = []
     i = 2
-    x = value
-
     if value == 2:
-        p_factors.append(2)
+        return [2]
     
-    else:
-
-        while value % i != 1:
-            if x % i == 0:
-                p_factors.append(i)
-                x = x / i
-            else:
-                i += 1
-    print(p_factors)
+    
 
 
 
-factors(2)
+print(factors(2))
