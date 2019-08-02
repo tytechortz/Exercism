8cdce0335f52fb3ba298e479fc01731574f851bb
James Swank <jmswank7@gmail.com>
2019-08-02 16:57:36 -0600

    passes tests

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,10 +1,17 @@
 def factors(value):
     i = 2
-    if value == 2:
-        return [2]
+    p_factors = []
+
+    if value <= 3 and value > 1:
+        return [value]
+    else:
+        while value > 1:
+            if value % i == 0:
+                p_factors.append(i)
+                value = value / i
+            else:
+                i += 1
     
-    
+    return p_factors
 
-
-
-print(factors(2))
+print(factors(1))
