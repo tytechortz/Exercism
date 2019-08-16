a1542ad9e98fe86c0087e24bd16d0500de0cdb6b
James Swank <jmswank7@gmail.com>
2019-08-16 15:16:39 -0600

    done?

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,10 +1,28 @@
 import numpy as np
 
 def saddle_points(matrix):
-
+    saddle_points = []
     matrix = np.asarray(matrix)
-    print(matrix)
+    print(len(matrix))
     print(matrix.shape)
-    for element in matrix.flat:
-        print(element)
-saddle_points([[3, 1, 3], [3, 2, 4]])
+    # print(matrix[1,1])
+    print(matrix[0])
+    print(matrix[:,0])
+
+    for x in range(len(matrix)):
+        for y in range(matrix.shape[1]):
+            if matrix[x][y] >= max(matrix[x]) and matrix[x][y] == min(matrix[:,y]):
+                saddle_points.append([x,y])
+    print(saddle_points)
+
+
+
+
+    # for x in range((matrix.shape[0])):
+    #     for y in range(matrix.shape[1])
+        
+    
+    # for element in range(len(matrix.shape[1])):
+    #     if element >= matrix
+        
+saddle_points([[9, 8, 7], [5, 3, 2]])
