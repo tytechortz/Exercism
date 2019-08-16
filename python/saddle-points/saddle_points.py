6c68951a4ed26d5e448990ce6ed495abb33af997
James Swank <jmswank7@gmail.com>
2019-08-16 15:35:41 -0600

    works?

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -4,17 +4,21 @@ def saddle_points(matrix):
     saddle_points = []
 
     matrix = np.asarray(matrix)
-    print(len(matrix))
-    print(matrix.shape)
+    # print(len(matrix))
+    # print(matrix.shape)
     # print(matrix[1,1])
-    print(matrix[0])
-    print(matrix[:,0])
-
-    for x in range(len(matrix)):
-        for y in range(matrix.shape[1]):
-            if matrix[x][y] >= max(matrix[x]) and matrix[x][y] == min(matrix[:,y]):
-                saddle_points.append({"row": x+1, "column": y+1})
-    print(saddle_points)
+    # print(matrix[0])
+    # print(matrix[:,0])
+    if len(matrix) == 0:
+        print([dict()])
+    elif len(matrix) == 1:
+        print([{"row":matrix[0], "column":matrix[0]}])
+    else:
+        for x in range(len(matrix)):
+            for y in range(matrix.shape[1]):
+                if matrix[x][y] >= max(matrix[x]) and matrix[x][y] == min(matrix[:,y]):
+                    saddle_points.append({"row": x+1, "column": y+1})
+        print(saddle_points)
 
     # for points in saddle_points:
 
@@ -29,4 +33,4 @@ def saddle_points(matrix):
     # for element in range(len(matrix.shape[1])):
     #     if element >= matrix
         
-saddle_points([[9, 8, 7], [5, 3, 2], [6, 6, 7]])
+saddle_points([[4, 5, 4], [3, 5, 5], [1, 5, 4]])
