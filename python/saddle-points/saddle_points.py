b406abefa3eb99bbb1f9b78e62f66c051d72e536
James Swank <jmswank7@gmail.com>
2019-08-16 14:12:43 -0600

    iterates over all elements

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -3,23 +3,8 @@ import numpy as np
 def saddle_points(matrix):
 
     matrix = np.asarray(matrix)
+    print(matrix)
     print(matrix.shape)
-    # print(matrix)
-    # print(matrix[2])
-    # print(matrix[:,0])
-    # print(max(matrix[0]))
-    answers = []
-    count = 0
-    # print(max(matrix[3//3]))
-    # print(min(matrix[:,0]))
-    for x in range(len(matrix)):
-        for y in matrix[x]:
-
-            # print(np.where(matrix == y))
-            if y == max(matrix[count//matrix.shape[0]]) and y == min(matrix[:,count % matrix.shape[0]]):
-                answers.append({"row": count//matrix.shape[0]+1, "column": count % matrix.shape[1]+1})
-            count += 1   
-    print(answers)
-    return answers
-
+    for element in matrix.flat:
+        print(element)
 saddle_points([[3, 1, 3], [3, 2, 4]])
