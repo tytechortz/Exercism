1285f23e19cfe42d707901812932df72824a1e0d
James Swank <jmswank7@gmail.com>
2019-08-18 16:11:01 -0600

    gets factors

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,5 +1,10 @@
 def classify(number):
-    pass
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+            
 
 
-classify(6)
+print(classify(6))
