f8a978d264e580aa8e6590aac51e8a24b906fd5f
James Swank <jmswank7@gmail.com>
2019-08-18 18:04:28 -0600

    might work

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,7 +4,19 @@ def classify(number):
         if number % i == 0:
             factors.append(i)
     return factors
+    
+def check(factors, number):
+        sum_of_factors = sum(factors[:-1])
+        if sum_of_factors == number:
+            return 'perfect'
+        elif sum_of_factors > number:
+            return "abundant"
+        else:
+            return "deficient"
+        # return sum_of_factors
             
 
 
-print(classify(6))
+
+# print(classify(6))
+# print(check(classify(30), 30))
