3f27ddc90dae1a71e7f69122bf4a4e5ceb0f1c5f
James Swank <jmswank7@gmail.com>
2019-08-18 18:07:34 -0600

    11 passed

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -3,20 +3,13 @@ def classify(number):
     for i in range(1, number + 1):
         if number % i == 0:
             factors.append(i)
-    return factors
     
-def check(factors, number):
-        sum_of_factors = sum(factors[:-1])
-        if sum_of_factors == number:
-            return 'perfect'
-        elif sum_of_factors > number:
-            return "abundant"
-        else:
-            return "deficient"
-        # return sum_of_factors
-            
+    sum_of_factors = sum(factors[:-1])
+    if sum_of_factors == number:
+        return 'perfect'
+    elif sum_of_factors > number:
+        return "abundant"
+    else:
+        return "deficient"
 
 
-
-# print(classify(6))
-# print(check(classify(30), 30))
