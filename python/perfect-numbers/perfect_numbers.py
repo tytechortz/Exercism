8d7e04ab5463df3ef99155fc7115bb560b7eef14
James Swank <jmswank7@gmail.com>
2019-08-18 18:11:52 -0600

    passes tests

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,15 +1,19 @@
 def classify(number):
     factors = []
-    for i in range(1, number + 1):
-        if number % i == 0:
-            factors.append(i)
-    
-    sum_of_factors = sum(factors[:-1])
-    if sum_of_factors == number:
-        return 'perfect'
-    elif sum_of_factors > number:
-        return "abundant"
+
+    if number <= 0:
+        raise ValueError("Must use positive integer")
     else:
-        return "deficient"
+        for i in range(1, number + 1):
+            if number % i == 0:
+                factors.append(i)
+        
+        sum_of_factors = sum(factors[:-1])
+        if sum_of_factors == number:
+            return 'perfect'
+        elif sum_of_factors > number:
+            return "abundant"
+        else:
+            return "deficient"
 
 
