b5bf6dc06f7b9e4d9906645c8f9159d9ab4f80fb
James Swank <jmswank7@gmail.com>
2019-08-19 09:07:17 -0600

    removes spaces and -

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,10 +2,12 @@ import re
 
 def is_isogram(string):
     string = string.lower()
-    string = re.sub("\s", "", string)
+    remove_chars = ' -'
+    for i in remove_chars:
+        string = string.replace(i, '')
     string_list = [char for char in string]
     
     return string_list
 
 
-print(is_isogram("Emily Jung Schwartzkopf"))
+print(is_isogram("Emily-Jung Schwartzkopf"))
